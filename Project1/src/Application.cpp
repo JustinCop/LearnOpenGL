7c1749b40a12d5dd491f9f0d1424d822cda481cd
zx <1019033041@qq.com>
2020-03-02 15:10:12 +0800

    initialize GLFW and GLEW

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -20,11 +22,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (GLEW_OK != glewInit())
+        std::cout << "ERROR: failed to initialize GLEW!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
